3b9cd8d1ac59c63b3a0d8c84a1c6da155b0be526
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-11-23 06:21:20 -0600

    First approach for the randomGenerator method

--- RandomNumbers/RandomNumbers.cpp
+++ RandomNumbers/RandomNumbers.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <random> //Contains C++11 random-number generation features
+
+void randomGenerator(std::size_t top, std::size_t down, std::size_t diewRolls) {
+    //engine that produces random numbers
+    std::default_random_engine engine{};
+
+    //distribution tha produces int vaues between 1 - n with equal likelihood
+    std::uniform_int_distribution randomDie{ top, down };
+
+    //display n ramdom die rolls
+    for (std::size_t counter{ 1 }; counter <= diewRolls; ++counter) {
+        std::cout << randomDie(engine) << " ";
+    }
+
+    std::cout << '\n';
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    randomGenerator(1, 8, 12);
+    return 0;
 }
